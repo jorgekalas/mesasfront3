9ad7880c79f45797c57077eb81c92db82e5524f1
jorgekalas <jorgekalas@gmail.com>
2023-03-16 21:04:03 -0300

    mesa 16-3

--- Mesa 15-3/api-jokes/src/components/Card.jsx
+++ Mesa 15-3/api-jokes/src/components/Card.jsx
@@ -4,6 +4,7 @@ const Card = ({type, setup, punchline}) => {
     return(
         <div className="card">
             <p>type: {type}</p>
+            <hr/>
             <p>{setup}</p>
             <p>{punchline}</p>
 
